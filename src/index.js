955ddcc441636a930f328c44a20a515005ab84c7
Gloria Guy <totallygloria@gmail.com>
2017-03-02 12:22:24 -0800

    Search bar successfully passes data to the movie rendering function
    Uses the props object to build a valid url from user input and then
    pass that to fetch to dynamically retrieve movie data and dump to
    the screen in ugly html.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.css';
 
+/*************** FIRST LANDING ***************/
+
+
+class MovieForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    console.log('A name was submitted: ', this.state );
+    getMovies(this.state);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={ this.handleSubmit }>
+        <label> Enter a movie to search for: 
+          <input type="text" value={ this.state.value } onChange={ this.handleChange } />
+        </label>
+          <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 const landing = (
-  <div>
+  <div className="box">
     <h1>Welcome to the Movie Display Demo</h1>
     <p>This is a simple React App to fetch movies and display the data.</p>
-    <div id="movielist">Loading movie data...</div>
-
+    <p>Search for a movie title to get started</p>
+    <MovieForm />
+    <div id="movielist"></div>
   </div>
 )
 
@@ -16,27 +51,31 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+/*************** FORM STUFF ***************/
+
 function MoviesHtml(props) {
 
-  let movies = props.movies;
-  console.log('prop movies', props);
-  let html = movies.map((movie, i) => {
+  let html = props.movies.map((movie, i) => {
     return <li key={'movie' + i}>{ movie.Title } ({ movie.Year })</li>
   })
 
-  console.log('completed html', html);
   return ( <ol>{ html }</ol> ) ;
 }
 
-fetch('http://www.omdbapi.com/?s=the%20princess%20bride')
-  .then(res => res.json())
-  .then(res => {
-    //let movieList = moviesHtml(res.Search);
-    console.log('movies are', res.Search);
-    ReactDOM.render(
-      <MoviesHtml movies={res.Search}/>,
-      document.getElementById('movielist')
-    )
-  })
-  .catch(err => console.log('Fetch error: ', err))
+function getMovies(state) {
 
+  let searchTerm = state.value.replace(/ /g, '%20')
+  let url = 'http://www.omdbapi.com/?s=' + searchTerm;
+  console.log('getMovies was called with: ', state.value);
+  console.log('fetch is called with: ', url);
+  fetch(url)
+    .then(res => res.json())
+    .then(res => {
+      ReactDOM.render(
+        <MoviesHtml movies={res.Search}/>,
+        document.getElementById('movielist')
+      )
+    })
+    .catch(err => console.log('Fetch error: ', err))
+}
