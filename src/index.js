9821259d7a9c1ce0854df64ce3527aeeacc86dd3
Gloria Guy <totallygloria@gmail.com>
2017-03-01 17:19:22 -0800

    Creates a basic react element which is rendered to the screen.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import './main.css';
+
+const landing = (
+  <div>
+    <h1>Welcome to the Movie Display Demo</h1>
+    <p>This is a simple React App to fetch movies and display the data.</p>
+    
+  </div>
+)
+
 
 ReactDOM.render(
-  <App />,
+  landing,
   document.getElementById('root')
 );
