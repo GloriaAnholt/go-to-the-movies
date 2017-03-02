a5c6ff54263d0b746dd4f0b3e1cf9a17a5b2ea83
Gloria Guy <totallygloria@gmail.com>
2017-03-01 17:47:28 -0800

    Saves attempt to create react elements and load to page. Not actually working.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,39 @@ const landing = (
   <div>
     <h1>Welcome to the Movie Display Demo</h1>
     <p>This is a simple React App to fetch movies and display the data.</p>
-    
+    <ul id="movielist">
+      <li>Loading movie data...</li>
+
+      </ul>
+
   </div>
 )
-
 
 ReactDOM.render(
   landing,
   document.getElementById('root')
 );
+
+function moviesHtml(movies) {
+
+  let html = movies.map((movie, i) => {
+    console.log(movie.Title, movie.Year);
+    return <li key={'movie' + i}>{ movie.Title }, { movie.Year }</li>
+  })
+
+  console.log('completed html', html);
+  return html;
+}
+
+fetch('http://www.omdbapi.com/?s=princess%20bride')
+  .then(res => res.json())
+  .then(movies => {
+    console.log('movies are', movies.Search);
+    let movieList = moviesHtml(movies.Search)
+    ReactDOM.render(
+      { movieList },
+      document.getElementById('movielist')
+    )
+  })
+  .catch(err => console.log('Fetch error: ', err))
+
